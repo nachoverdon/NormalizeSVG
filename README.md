62b7193285b1fbe3ce311ec2e751dfbde11055fb
Nacho Verdón <nverblaz@gmail.com>
2019-04-17 18:38:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What
+Tool to prepare .SVG for a Lime/OpenFL project for .SVGs exported from Photoshop
+
+# How to
+
+1 - Build
+2 - Execute passing path to .svg as argument
+3 - Get new .svg on the same folder
---
+++ src/Main.hx
@@ -0,0 +1,110 @@
+import haxe.xml.Access;
+import sys.io.File;
+
+using StringTools;
+
+
+class Main {
+	static var xmlFile: Xml;
+	static var xml: Access;
+	static var path: Null<String>;
+
+	static function main() {
+		readXML();
+
+		var style: String = xml.node.defs.node.style.innerData;
+		var classes = searchStyleClasses(style);
+		for (key => value in classes) {
+			classes.set(key, parseStyleClass(style, key));
+		}
+
+		for (clKey => cls in classes) {
+			for (rule => value in cls) {
+				for (path in xmlFile.elementsNamed('path')) {
+					var classAttr = path.get('class');
+					if (classAttr == null || '.$classAttr' != clKey) continue;
+					// path.remove('class');
+					path.set(rule, value);
+				}
+			}
+		}
+
+		xmlFile.removeChild(xmlFile.elementsNamed('defs').next());
+		xmlFile.removeChild(xmlFile.elementsNamed('metadata').next());
+
+		File.saveContent('${path}_normalized.svg', xmlFile.toString());
+	}
+
+	static function readXML() {
+		var args = Sys.args();
+		path = args.length > 0 ? args[0] : null;
+
+		if (path == null) {
+			trace('[Must specify a file path.]');
+			Sys.exit(0);
+		}
+
+		var file = File.getContent(path);
+		xmlFile = Xml.parse(file).firstElement();
+		xml = new Access(xmlFile);
+	}
+
+	static function parseStyleClass(style: String, name: String): Map<String, String> {
+		final CLASS = 'CLASS';
+		var regex = new EReg('(\\$name)(?=[,\\s\\{])', 'gi');
+		style = regex.replace(style, CLASS);
+		var idx = style.indexOf(CLASS);
+		var map = new Map<String, String>();
+
+		while (idx > -1) {
+			var opBracketIdx = style.indexOf('{', idx);
+			var clBracketIdx = style.indexOf('}', idx);
+			var rules = style.substring(opBracketIdx + 1, clBracketIdx);
+			rules = rules.replace(': ', ' ').replace(';', '');
+			var rulesArray = rules.split('\n');
+
+			for (i in 0...rulesArray.length) rulesArray[i] = rulesArray[i].trim();
+
+			while (rulesArray.remove('')) {}
+
+			for (rule in rulesArray) {
+				var r = rule.trim().split(' ');
+
+				map.set(r[0], r[1]);
+			}
+
+			idx = style.indexOf(CLASS, clBracketIdx);
+		}
+
+		// trace(map);
+		return map;
+	}
+
+	static function searchStyleClasses(style: String): Map<String, Map<String, String>> {
+		function getMatches(ereg:EReg, input:String, index:Int = 0):Array<String> {
+			var matches = [];
+
+			while (ereg.match(input)) {
+				matches.push(ereg.matched(index));
+				input = ereg.matchedRight();
+			}
+
+			return matches;
+		}
+
+		var classes = new Map<String, Map<String, String>>();
+
+		var regex = ~/(\.cls-\d+)(?=[,\s\{])/gi;
+
+		var matches = getMatches(regex, style);
+
+		for (match in matches) {
+			if (classes.exists(match)) continue;
+
+			var rules = new Map<String, String>();
+			classes.set(match, rules);
+		}
+
+		return classes;
+	}
+}
